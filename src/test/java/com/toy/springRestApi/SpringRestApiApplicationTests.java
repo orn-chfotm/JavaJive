2abe79f05974980686418889f931cf5b9ccf31a8
orn-chfotm <chfotm@gmail.com>
2024-02-04 17:59:04 +0900

    2024-02-04 프로젝트 명 변경

--- src/test/java/com/toy/springRestApi/SpringRestApiApplicationTests.java
+++ src/test/java/com/toy/springRestApi/SpringRestApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.toy.javajive;
+package com.toy.springRestApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaJiveApplicationTests {
+class SpringRestApiApplicationTests {
 
     @Test
     void contextLoads() {
--- src/test/java/com/toy/springRestApi/repository/MemberRepositoryTest.java
+++ src/test/java/com/toy/springRestApi/repository/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
-package com.toy.javajive.repository;
+package com.toy.springRestApi.repository;
 
-import com.toy.javajive.entity.MemberEntity;
-import org.junit.jupiter.api.Assertions;
+import com.toy.springRestApi.entity.MemberEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
