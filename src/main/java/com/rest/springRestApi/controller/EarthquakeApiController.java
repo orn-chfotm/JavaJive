0a2ac1241e2f78f1b61388472f43d73fb982b4bc
orn-chfotm <chfotm@gmail.com>
2024-02-08 00:41:55 +0900

    2024-02-08 Api 호출 구성 생성

--- src/main/java/com/rest/springRestApi/controller/EarthquakeApiController.java
+++ src/main/java/com/rest/springRestApi/controller/EarthquakeApiController.java
@@ -2,17 +2,27 @@ package com.rest.springRestApi.controller;
 
 import com.rest.springRestApi.data.dto.EarthQuakeDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <pre>
+ *  행정안전부_지진 옥외대피장소 API
+ * </pre>
+ *
+ * @ClassName: EarthquakeApiController
+ * @Description: 행정안전부_지진 옥외대피장소 API Controller
+ */
 @RestController
+@RequestMapping(value = "/earthQuake")
 public class EarthquakeApiController {
 
     @GetMapping(value = "/getList")
-    public List<EarthQuakeDto> getList() {
-        List<EarthQuakeDto> list = new ArrayList<>();
+    public List<? extends EarthQuakeDto> getList() {
+        List<? extends EarthQuakeDto> list = new ArrayList<>();
 
         return list;
     }
