a727cb9119db5f78bc2d37c815c778de3f92b4b3
orn-chfotm <chfotm@gmail.com>
2024-02-09 19:44:59 +0900

    2024-02-09 Api 호출 구성 Test

--- src/main/java/com/rest/springRestApi/service/impl/EarthquakeApiServiceImpl.java
+++ src/main/java/com/rest/springRestApi/service/impl/EarthquakeApiServiceImpl.java
@@ -1,10 +1,16 @@
 package com.rest.springRestApi.service.impl;
 
+import com.rest.springRestApi.data.dto.request.ApiExplorerRequest;
 import com.rest.springRestApi.data.dto.response.EarthQuakeResponse;
 import com.rest.springRestApi.service.EarthquakeApiService;
+import com.rest.springRestApi.util.ApiExplorer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +18,10 @@ import java.util.List;
 public class EarthquakeApiServiceImpl implements EarthquakeApiService {
 
     @Override
-    public List<EarthQuakeResponse> getList() {
-        return null;
+    public List<EarthQuakeResponse> getList(ApiExplorerRequest apiExplorerRequest) throws IOException, JSONException {
+        ApiExplorer.getConApi(apiExplorerRequest);
+
+        List<EarthQuakeResponse> list = new ArrayList<>();
+        return list;
     }
 }
--- src/main/java/com/rest/springRestApi/util/ApiExplorer.java
+++ src/main/java/com/rest/springRestApi/util/ApiExplorer.java
@@ -1,7 +1,20 @@
 package com.rest.springRestApi.util;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.core.JsonParser;
+import com.rest.springRestApi.data.dto.request.ApiExplorerRequest;
+import lombok.Value;
+import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.ui.Model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * <pre>
@@ -11,7 +24,67 @@ import org.springframework.context.annotation.Configuration;
  * @ClassName: ApiExplorer
  * @Description: 공공데이터 포털 통신 Util Class
  */
-public class ApiExplorer<T> {
 
+@Log4j2
+public class ApiExplorer{
+
+    public static void getConApi(ApiExplorerRequest apiExplorerRequest) throws IOException {
+        final String UNC = "UTF-8";
+        // Api Url
+        //StringBuilder urlBuilder = new StringBuilder(apiExplorerRequest.getUrl());
+        StringBuilder urlBuilder = new StringBuilder("https://apis.data.go.kr/1741000/EmergencyAssemblyArea_Earthquake3/getArea3List");
+
+        urlBuilder.append("?").append(URLEncoder.encode("ServiceKey", UNC)).append("=").append("%2Bn9ZwKaEA0%2FZzATwHrLLjpzLtJNyZQnIGhoOQYgZQ65rUGySka3qW95tM9lhzAaCszxEOaphTYE9DSF%2FNNFgaQ%3D%3D");
+        urlBuilder.append("&").append(URLEncoder.encode("pageNo", UNC)).append("=").append(URLEncoder.encode("1", UNC));
+        urlBuilder.append("&").append(URLEncoder.encode("numOfRows", UNC)).append("=").append(URLEncoder.encode("10", UNC));
+        urlBuilder.append("&").append(URLEncoder.encode("type", UNC)).append("=").append(URLEncoder.encode("JSON", UNC));
+
+        //urlBuilder.append("?").append(URLEncoder.encode("ServiceKey", UNC)).append("=").append(apiExplorerRequest.getServiceKey());
+        //urlBuilder.append("&").append(URLEncoder.encode("pageNo", UNC)).append("=").append(URLEncoder.encode(apiExplorerRequest.getPageNo(), UNC));
+        //urlBuilder.append("&").append(URLEncoder.encode("numOfRows", UNC)).append("=").append(URLEncoder.encode(apiExplorerRequest.getNumOfRows(), UNC));
+        //urlBuilder.append("&").append(URLEncoder.encode("type", UNC)).append("=").append(URLEncoder.encode(apiExplorerRequest.getType(), UNC));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+        urlConn.setRequestMethod("GET");
+        urlConn.setRequestProperty("Content-type", "application/json");
+
+        log.info("Response Code :: " + urlConn.getResponseCode());
+
+        BufferedReader br;
+
+        if (urlConn.getResponseCode() >= 200 && urlConn.getResponseCode() <= 300) {
+            br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(urlConn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        System.err.println(sb.toString());
+
+        br.close();
+        urlConn.disconnect();
+        /*
+        JSONArray dataArr = null;
+        try {
+            // 1. 문자열 형태의 JSON을 파싱하기 위한 JSONParser 객체 생성.
+            JSONParser parser = new JSONParser(sb.toString());
+            // 2. 문자열을 JSON 형태로 JSONObject 객체에 저장.
+            JSONObject obj = (JSONObject) parser.parse();
+            // 3. 필요한 리스트 데이터 부분만 가져와 JSONArray로 저장.
+            dataArr = (JSONArray) obj.get("row");
+            // 4. model에 담아준다.
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        return dataArr;
+        */
+    }
 
 }
